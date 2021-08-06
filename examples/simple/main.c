cde86d01cda2ea6ad73838e908c07f04a5918876
basavesh <basavesh.shivakumar@gmail.com>
2021-08-06 11:32:55 +0200

    use secdec with inline asm fence

--- examples/simple/main.c
+++ examples/simple/main.c
@@ -6,6 +6,11 @@
 const char *base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ\
 abcdefghijklmnopqrstuvwxyz0123456789+/";
 
+uint64_t secdec(uint64_t input) {
+  asm("lfence":::"memory");
+  return input;
+}
+
 int main(void) {
   uint64_t key = 0x1d381f22be58ac3a;
   uint64_t msg = 0x09a9d3591c6adb40;
